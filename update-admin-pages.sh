5845b70181eceb4247995c3b02e6181b835e5286
Brandon Johnson <brandon.johnson@space150.com>
2017-04-02 14:26:20 -0500

    Update icons and build scripts

--- update-admin-pages.sh
+++
@@ -1,3 +0,0 @@
-curl https://raw.githubusercontent.com/JavaScriptMN/JavaScriptMN/master/CODE_OF_CONDUCT.md -o pages/code-of-conduct.md ;
-curl https://raw.githubusercontent.com/JavaScriptMN/JavaScriptMN/master/Speaker_Questionnaire.md -o pages/speak.md ;
-curl https://raw.githubusercontent.com/JavaScriptMN/JavaScriptMN/master/Sponsorship_Opportunities.md -o pages/sponsor.md ;
