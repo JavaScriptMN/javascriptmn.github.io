353005df0744b2c5f1ce724ac28275687cb216d3
Brandon Johnson <b@johnsonmn.org>
2017-12-02 16:11:18 -0600

    Use prettier

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Run `npm run develop` and access the dev server at the address it reports.
 Run `npm build`, and a built version will be written to `/dist`. This will
 also update the markdown files that define our code of conduct, speaker
 questionnaire, and sponsorship levels from the [admin repository](https://github.com/javascriptmn/javascriptmn)
+
+## Style
+This repository is configured to use [prettier](https://prettier.io/) on commit.
+
+It will lint and format your changes automatically, so don't worry about following
+format standards (so long as your code passes the lint-staged step).
--- components/Meetup.js
+++ components/Meetup.js
@@ -9,73 +9,85 @@ export default class Meetup extends React.Component {
   state = {
     name: '',
     time: Date.now(),
-    venue: {name: ''},
+    venue: { name: '' },
     loading: false,
     error: false
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.setState({
       loading: true,
       error: false
     })
-    window.fetch(NEXT_MEETUP_URI)
-      .then((res) => {
+    window
+      .fetch(NEXT_MEETUP_URI)
+      .then(res => {
         // Gotta love network errors...
         if (!res.ok) throw Error(response.statusText)
         return response
       })
-      .then((res) => res.json())
-      .then((json) => this.setState({
-        ...json,
-        loading: false
-      }))
-      .catch(_ => this.setState({loading: false, error: true}))
+      .then(res => res.json())
+      .then(json =>
+        this.setState({
+          ...json,
+          loading: false
+        })
+      )
+      .catch(_ => this.setState({ loading: false, error: true }))
   }
 
-  render () {
+  render() {
     const meetupStyles = {
-        container: {
-          textAlign: 'center',
-          border: '1px solid black',
-          display: 'inline block',
-          margin: '8rem'
-        },
-        title: {
-          fontSize: '2rem',
-          margin: '1rem'
-        },
-        bug: {},
-        rsvpButton: {}
+      container: {
+        textAlign: 'center',
+        border: '1px solid black',
+        display: 'inline block',
+        margin: '8rem'
+      },
+      title: {
+        fontSize: '2rem',
+        margin: '1rem'
+      },
+      bug: {},
+      rsvpButton: {}
     }
 
-    const MeetupDescription = ({name, time, venue}) => (<div className="next-event">
+    const MeetupDescription = ({ name, time, venue }) => (
+      <div className="next-event">
         <div className="next-event-tagline">
           <strong>Next Event:</strong>
-          <div className="event-name">
-            {name}
-          </div>
-          {
-            (time && venue && venue.name) &&
-            (<span className="next-event-timestamp">{format(new Date(time), 'dddd, MMMM Do, YYYY')} at <strong>{ venue.name }</strong></span>)
-          }
+          <div className="event-name">{name}</div>
+          {time &&
+            venue &&
+            venue.name && (
+              <span className="next-event-timestamp">
+                {format(new Date(time), 'dddd, MMMM Do, YYYY')} at{' '}
+                <strong>{venue.name}</strong>
+              </span>
+            )}
         </div>
-      </div>)
+      </div>
+    )
 
-    if (this.state.loading)
-      return (<MeetupDescription
-          name={'Loading...'}
-         />)
+    if (this.state.loading) return <MeetupDescription name={'Loading...'} />
 
     if (this.state.error)
-      return (<MeetupDescription
-          name={(<a href='https://meetup.com/javascriptmn'>Check Meetup.com for updates</a>)}
-         />)
+      return (
+        <MeetupDescription
+          name={
+            <a href="https://meetup.com/javascriptmn">
+              Check Meetup.com for updates
+            </a>
+          }
+        />
+      )
 
-    return (<MeetupDescription
+    return (
+      <MeetupDescription
         name={this.state.name}
         time={this.state.time}
         venue={this.state.venue}
-      />)
+      />
+    )
   }
 }
