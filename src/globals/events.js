4b3ff7190e830ded220dbc6df6e8ad0c2d74b6c4
Brian Mitchell <bman4789@gmail.com>
2020-10-21 21:31:49 -0500

    Update event title

--- src/globals/events.js
+++ src/globals/events.js
@@ -7,7 +7,7 @@ module.exports = [
     hidden: true,
   },
   {
-    title: 'October Meetup',
+    title: 'October Event: Shalanah Dawson, Ryan Rampersad, Brian Mitchell',
     date: new Date('2020-10-28T18:00-05:00'),
     link: 'https://www.meetup.com/JavaScriptMN/events/hmzgxrybcnblc/',
     location: 'Virtual',
