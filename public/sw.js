a1b9776ef5b99a4f0d21a31e916a0f04fe217f30
Brian Mitchell <bman4789@gmail.com>
2020-10-26 00:42:14 -0500

    Attempt to purge the old site with a self-destructing service worker

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+// cleanup old Gatsby ServiceWorkers
+// https://github.com/NekR/self-destroying-sw
+self.addEventListener('install', function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll();
+    })
+    .then(function (clients) {
+      clients.forEach((client) => client.navigate(client.url));
+    });
+});
