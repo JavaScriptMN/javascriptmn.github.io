50ab4e86be91551df9e2f5d7cb79934a6fc84138
Brian Mitchell <bman4789@gmail.com>
2020-10-25 21:34:04 -0500

    Use a more appropriate element removal method

--- src/includes/events.js
+++ src/includes/events.js
@@ -5,11 +5,10 @@
     const time = new Date(
       event.querySelector('time[datetime]').getAttribute('datetime')
     );
-    // hide the event until the end of the day it's scheduled for
     time.setHours(23, 59, 59, 999);
+    // remove future events
     if (Date.now() < time.getTime()) {
-      // TIL this is a thing, wow!
-      event.outerHTML = '';
+      event.remove();
     }
   }
 })();
