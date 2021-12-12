bfd060f1c256300e35097f4a12943226b6c1d9e1
Ryan Rampersad <ryan.rampersad@gmail.com>
2021-12-12 12:35:10 -0600

    Adds organizers.js data

---
+++ src/globals/organizers.js
@@ -0,0 +1,22 @@
+/**
+ * @typedef JSMNOrganizer
+ * @type {object}
+ * @property {string} name - name of organizer
+ * @property {string} avatar - avatar url of organizer
+ */
+
+/**
+ * @type {JSMNOrganizer[]}
+ */
+module.exports = [
+  {
+    name: 'Brian Mitchell',
+    avatar:
+      'https://www.gravatar.com/avatar/89e0d7d3d9370c45517960c8a12f92b9?size=400',
+  },
+  {
+    name: 'Brandon Johnson',
+    avatar:
+      'https://www.gravatar.com/avatar/1b3b4a36d37e80d04c3b0a9185172541?s=400',
+  },
+];
