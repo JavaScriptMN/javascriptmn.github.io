676f1980d56d8b841fb882b6b8fa7d570659ffed
Brian Mitchell <bman4789@gmail.com>
2022-05-23 10:37:25 -0500

    Add May 2022 event

--- src/globals/events.js
+++ src/globals/events.js
@@ -13,6 +13,12 @@
  */
 module.exports = [
   {
+    title: 'May Event: Show and Tell',
+    date: new Date('2022-05-25T18:00-05:00'),
+    link: 'https://www.meetup.com/JavaScriptMN/events/qltnfsydchbhc/',
+    location: 'Virtual',
+  },
+  {
     title: 'April Event: Show and Tell',
     date: new Date('2022-04-27T18:00-05:00'),
     link: 'https://www.meetup.com/JavaScriptMN/events/qltnfsydcgbkc/',
