d7582a95fa44129a2bfce5316ab5ca91dfb5f06c
Brian Mitchell <bman4789@gmail.com>
2023-03-29 22:20:46 -0500

    Consider 9 PM the end of the day instead of midnight

--- src/includes/events.js
+++ src/includes/events.js
@@ -5,7 +5,8 @@
     const time = new Date(
       event.querySelector('time[datetime]').getAttribute('datetime')
     );
-    time.setHours(23, 59, 59, 999);
+    // consider 9 PM the end of the day
+    time.setHours(20, 59, 59, 999);
     // remove future events
     if (Date.now() < time.getTime()) {
       event.remove();
