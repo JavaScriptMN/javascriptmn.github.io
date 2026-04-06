6b0c9f218182673ebe8fc0334e9273172530da4b
Brian Mitchell <bman4789@gmail.com>
2026-04-06 09:27:06 -0500

    Add April 2026 event

--- src/globals/events.js
+++ src/globals/events.js
@@ -13,6 +13,12 @@
  */
 module.exports = [
   {
+    title: "JavaScriptMN In-Person at Target Plaza Commons",
+    date: new Date("2026-04-15T18:00-05:00"),
+    link: "https://www.meetup.com/javascriptmn/events/313105536/",
+    location: "Target Plaza Commons, 1001 Nicollet Mall, Minneapolis, MN 55403",
+  },
+  {
     title: "February Event: Open Floor Show/Tell and Discussion",
     date: new Date("2026-02-25T18:00-06:00"),
     link: "https://www.meetup.com/javascriptmn/events/313105536/",
